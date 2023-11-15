1be49546aebfcc557f95b17b5cc24d9f6f988c93
Rohan-Balkondekar-Lyzr <rohan@lyzr.ai>
2023-11-15 13:46:22 +0530

    Added DataAnalyzr

---
+++ cookbook/DataAnalyzr/demo.py
@@ -0,0 +1,12 @@
+from lyzr import DataAnalyzr
+import pandas as pd
+import openai
+
+openai.api_key = "REDACTED_SET_OPENAI_API_KEY_ENV"
+
+df = pd.read_csv("./cars_prices.csv")
+user_query = "what is the impact of milage on the price of 2wd cars"
+
+da = DataAnalyzr(df, user_query)
+model_response = da.getCode()
+exec(model_response)
